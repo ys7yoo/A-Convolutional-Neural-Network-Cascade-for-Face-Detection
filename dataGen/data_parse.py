3b2a40f26ae6100feb6246545ee599e930a26b22
YY <ys7yoo@gmail.com>
2018-04-10 18:56:33 +0900

    converted to Python3

--- dataGen/data_parse.py
+++ dataGen/data_parse.py
@@ -1,7 +1,7 @@
 import sqlite3
 
-db_dir = "/media/sda1/Data/Face_detection/AFLW/aflw/data/aflw.sqlite"
-save_dir = "/media/sda1/Data/Face_detection/AFLW/aflw/data/annot"
+db_dir = "/var/data/AFLW/aflw/data/aflw.sqlite"
+save_dir = "/var/data/AFLW/aflw/data/annot"
 
 con = sqlite3.connect(db_dir)
 cursor = con.cursor()
@@ -9,10 +9,17 @@ cursor = con.cursor()
 cursor.execute("select FaceImages.filepath, FaceRect.x, FaceRect.y, FaceRect.w, FaceRect.h from FaceImages join Faces on FaceImages.file_id = Faces.file_id join FaceRect on FaceRect.face_id = Faces.face_id")
 
 result = cursor.fetchall()
+# (filename, x, y, w, h)
+#print(result)
 
-#filename, x, y, w, h
-fp = open(save_dir,"w")
-for elem in result:
-    print>>fp, elem
+with open(save_dir, "w") as out:
+    for elem in result:
+        out.write(elem[0]+', ')
+        out.write('{}, {}, {}, {}\n'.format(elem[1], elem[2], elem[3], elem[4]))
+    
+# below is Python 2 version
+#fp = open(save_dir,"w")
+#for elem in result:
+#    print>>fp, elem
 
 
